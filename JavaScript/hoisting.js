f1c0b55f852128e4e6339b64ed7e86f813266a2f
Matheus Vicente <vicentematheus1507@gmail.com>
2026-03-09 11:53:15 -0300

    feat: add hoisting example with variable declaration and console logs

---
+++ JavaScript/hoisting.js
@@ -0,0 +1,8 @@
+console.log("Minha idade atual é: ", idade);
+
+var idade = 25 
+//variavel globar nao pode ser referenciada antes da declaração
+//var pode
+//let e const não podem ser referênciadas antes da inicialização, por isso se usa mais eles, para nao dar risco de aparecer algum bug futuro por usar uma variavel undefined, ele ja quebra na hora
+
+console.log("Minha idade atual depois de declarar: ", idade);
