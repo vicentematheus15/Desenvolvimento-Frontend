0b7172250c025401261adad27eb5a109362d6635
Matheus Vicente <vicentematheus1507@gmail.com>
2026-03-09 13:34:47 -0300

    feat: add type checking examples for various data types in JavaScript

---
+++ JavaScript/introducao/tipos.js
@@ -0,0 +1,17 @@
+let nome = "Ana"
+let idade = 32
+let altura = 1.77
+console.log(typeof nome );
+console.log(typeof idade);
+console.log(typeof altura);
+
+
+let estaChovendo = true
+console.log(typeof estaChovendo);
+
+
+let cidade
+console.log(typeof cidade);
+
+let aprendeuJS = null
+console.log(typeof aprendeuJS);
