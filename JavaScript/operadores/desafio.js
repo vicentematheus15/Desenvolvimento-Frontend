83856d32fe21b39488962ebb0a9ec2580237687e
Matheus Vicente <vicentematheus1507@gmail.com>
2026-03-12 14:45:15 -0300

    feat: add arithmetic operators example with basic operations and increment/decrement

---
+++ JavaScript/operadores/desafio.js
@@ -0,0 +1,13 @@
+console.log("------ Boas vindas a calculadora de trocos! ------");
+
+let valorCompra = 18.50
+let valorPago = 20
+
+console.log(`Valor da compra: R$${valorCompra.toFixed(2)}`);
+console.log(`Valor recebico: R$${valorPago.toFixed(2)}`);
+
+let troco = valorPago - valorCompra
+
+console.log(`Troco a devolver: R$${troco.toFixed(2)}`);
+
+
