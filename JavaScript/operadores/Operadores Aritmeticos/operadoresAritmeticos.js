b39322f11d25dd8724552a91f1aa2c441ac50924
Matheus Vicente <vicentematheus1507@gmail.com>
2026-03-12 10:17:37 -0300

    feat: add arithmetic operators example with basic operations and increment/decrement

---
+++ JavaScript/operadores/Operadores Aritmeticos/operadoresAritmeticos.js
@@ -0,0 +1,23 @@
+const numero1 = 8
+const numero2 = 4
+
+const soma = numero1 + numero2
+const subtracao = numero1 - numero2
+const multiplicaao = numero1 * numero2
+const divisao = numero1 / numero2
+
+const resultados = `
+Soma: ${soma}
+Subtração: ${subtracao}
+Multiplicação: ${multiplicaao}
+Divisão: ${divisao}`
+
+console.table(resultados)
+
+
+let contador = 5;
+contador++;
+console.log('Incremento: ', contador);
+
+contador--;
+console.log('Decremento: ', contador);
