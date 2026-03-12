dccbb9041424bbe9ee9a5762071c5646a4465761
Matheus Vicente <vicentematheus1507@gmail.com>
2026-03-12 11:43:57 -0300

    feat: add comparison operators example with strict and loose equality checks

---
+++ JavaScript/operadores/operadoresComparacao.js
@@ -0,0 +1,18 @@
+const idadeVinny = 36;
+const minhaIdade = 25;
+
+
+// const saoIguais = idadeVinny == minhaIdade;
+console.log('São iguais? ', saoIguais);
+
+// const comparacao = idadeVinny > minhaIdade;
+console.log('É maior? ', comparacao);
+
+// const comparacao = idadeVinny < minhaIdade;
+console.log('É menor? ', comparacao);
+
+const comparacaoEstrita = minhaIdade === idadeVinny;
+console.log('São estritamente iguais?', comparacaoEstrita);
+
+const comparacaoDiferente = minhaIdade !== idadeVinny;
+console.log('São estritamente diferentes?', comparacaoDiferente);
