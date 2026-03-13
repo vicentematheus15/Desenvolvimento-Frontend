50ea3b38101d540260ff0e1cc5951a101b573367
Matheus Vicente <vicentematheus1507@gmail.com>
2026-03-13 15:24:17 -0300

    feat: add greeting and double calculation functions

---
+++ JavaScript/funcoes/funcoes.js
@@ -0,0 +1,13 @@
+function saudacao(nome) {
+    console.log('Olá', nome)   
+}
+
+saudacao('Vicente');
+
+
+function calcularDobroDeUm(numero) {
+    return numero * 2
+}
+
+const numeroDobrado = calcularDobroDeUm(4)
+console.log(numeroDobrado);
