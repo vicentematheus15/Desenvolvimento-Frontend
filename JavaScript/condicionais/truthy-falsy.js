346c4ac0c2b3029b56c915f810f1cf3ddfcf1900
Matheus Vicente <vicentematheus1507@gmail.com>
2026-03-13 10:55:27 -0300

    fix: improve clarity of falsy and truthy evaluation comments

--- JavaScript/condicionais/truthy-falsy.js
+++ JavaScript/condicionais/truthy-falsy.js
@@ -1,5 +1,5 @@
 //transforma a condicao do if em true ou false. se for true, entra no if, se for false segue sem entrar
-// esses valores sempre retornarão falsy:
+// esses valores sempre sao considerados falsos (falsy):
 // - false
 // - 0 (o número zero)
 // - "" (string vazia)
@@ -7,7 +7,7 @@
 // - undefined
 // - NaN (Not a Number)
 
-//todo o resto sempre retorna true
+//todo o resto sempre é considerado (truthy) e retorna true
 
 const nome = '';
 
