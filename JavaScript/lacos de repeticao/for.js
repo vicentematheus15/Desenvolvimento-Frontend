74c159cdc5157b087d132ef582b13cb082842996
Matheus Vicente <vicentematheus1507@gmail.com>
2026-03-13 11:59:51 -0300

    feat: add for loop examples for number iteration and string character access

---
+++ JavaScript/lacos de repeticao/for.js
@@ -0,0 +1,19 @@
+for(let contador = 1; contador <= 5; contador++) {
+    console.log('Número atual:', contador)
+}
+
+for(let numero = 0; numero <= 15; numero++){
+    if (numero % 2 == 0) {
+        console.log('Número par encontrado:', numero)
+    }
+}
+
+for(let numero = 0; numero <= 15; numero++){
+    if (numero % 2 > 0) {
+        console.log('Número Ímpar encontrado:', numero)
+    }
+}
+
+for(let contador = 0; contador < palavra.length; contador++) {
+    console.log(palavra[contador])
+}
