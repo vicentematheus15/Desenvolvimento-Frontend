577af4f11aa9b5ccc153cfaa81036e20723bde2e
Matheus Vicente <vicentematheus1507@gmail.com>
2026-03-13 10:31:55 -0300

    feat: add grading system example with conditional statements

--- JavaScript/condicionais/condicionais.js
+++ JavaScript/condicionais/condicionais.js
@@ -5,3 +5,15 @@ if(idade >= 18){
 }else{
     console.log("É menor de idade");
 }
+
+const notaDoAluno = 8;
+
+if (notaDoAluno >= 9) {
+    console.log('Nota excelente');
+} else if (notaDoAluno >= 7) {
+    console.log('Nota boa');
+}else if (notaDoAluno >= 4) {
+    console.log('Nota mediana');
+} else {
+    console.log('Nota ruim');
+}  
