9fe3280d05601122709616520b860a16cb5ddbe1
Matheus Vicente <vicentematheus1507@gmail.com>
2026-03-13 22:15:34 -0300

    arrrowFunction examples

---
+++ JavaScript/funcoes/arrowFunction.js
@@ -0,0 +1,19 @@
+//JEITO NORMAL
+// function calcularDobroDeUm(numero) {
+//     return numero * 2
+// }
+// const numeroDobrado = calcularDobroDeUm(4)
+// console.log(numeroDobrado);
+
+
+//ARROW FUNCTION
+// const calcularDobroDeUm = (numero) => {
+//     return numero * 2
+// }
+
+//ARROW FUNCTION SIMPLIFICADA
+const calcularDobroDeUm = numero => numero * 2
+
+
+const numeroDobrado = calcularDobroDeUm(4)
+console.log('O dorbro de 4 é: ', numeroDobrado);
