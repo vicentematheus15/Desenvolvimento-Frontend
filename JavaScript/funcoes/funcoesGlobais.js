0fd6a59e2096bba9ab16dfce639cc2cb9ea7ae85
Matheus Vicente <vicentematheus1507@gmail.com>
2026-03-18 11:22:17 -0300

    fix: comment out setTimeout call in saudacao function and ensure proper formatting

--- JavaScript/funcoes/funcoesGlobais.js
+++ JavaScript/funcoes/funcoesGlobais.js
@@ -2,6 +2,14 @@ function saudacao(){
     console.log('E ai, beleza?');
 }
 
-setTimeout(saudacao, 2000); // 2000 milisegundos
+//setTimeout(saudacao, 2000); // 2000 milisegundos
 
 let contador = 0;
+
+const id = setInterval(()=> {
+    contador++
+    console.log("Tempo decorrido(sem segundos):", contador);
+    if(contador == 10){
+        clearInterval(id)
+    }
+}, 1000)
