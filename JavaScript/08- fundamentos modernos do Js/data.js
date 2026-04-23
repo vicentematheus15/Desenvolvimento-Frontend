973c3603974fc18a8380e08f29a0a00c1f8ef072
Matheus Vicente <vicentematheus1507@gmail.com>
2026-04-23 13:32:04 -0300

    feat: add data.js to log current date and time details

---
+++ JavaScript/08- fundamentos modernos do Js/data.js
@@ -0,0 +1,8 @@
+const agora = new Date();
+
+console.log(agora);
+console.log('Ano: ',agora.getFullYear() );
+console.log('Mês 0-11: ',agora.getMonth() );
+console.log('Dia do mês: ',agora.getDate() );
+console.log('Hora: ',agora.getHours() );
+console.log('Minutos: ',agora.getMinutes() );
