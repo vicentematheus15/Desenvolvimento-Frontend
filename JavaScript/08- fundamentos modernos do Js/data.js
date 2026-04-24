fb8549ef81350753832099f84e1d65ac7ad298dc
vicentematheus15 <vicentematheus1507@gmail.com>
2026-04-24 16:46:35 -0300

    feat: enhance data.js to log formatted birth date and current date details

--- JavaScript/08- fundamentos modernos do Js/data.js
+++ JavaScript/08- fundamentos modernos do Js/data.js
@@ -6,3 +6,8 @@ console.log('Mês 0-11: ',agora.getMonth() );
 console.log('Dia do mês: ',agora.getDate() );
 console.log('Hora: ',agora.getHours() );
 console.log('Minutos: ',agora.getMinutes() );
+
+const nascimento = new Date('2000-04-15:10:00.000Z');
+console.log(nascimento);
+
+console.log('Data formatada: ', nascimento.toLocaleDateString('pt-BR'));
