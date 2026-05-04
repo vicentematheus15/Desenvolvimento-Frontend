262a408b9ef5cdfffbb4acb055fba20c7b18c8af
vicentematheus15 <vicentematheus1507@gmail.com>
2026-05-04 17:19:30 -0300

    feat: implement basic calculator functionality with addition and subtraction operations

---
+++ JavaScript/08- fundamentos modernos do Js/calculadora/index.js
@@ -0,0 +1,32 @@
+import { createInterface } from 'readline';
+import { soma, subtracao } from './operacoesMatematicas.js';
+
+const leitor = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question('Digite o primeiro número:\n>', (numero1) => {
+    leitor.question('Digite a operação:\n+: Soma\n-: Subtração\n>', (operacao) => {
+        leitor.question('Digite o segundo número:\n>', (numero2) => {
+            const num1 = Number(numero1);
+            const num2 = Number(numero2);
+
+            let resultado = null;
+
+            if (operacao == '+') {
+                resultado = soma(num1, num2);
+            } else if (operacao == '-') {
+                resultado = subtracao(num1, num2);
+            } else {
+                console.log('Operação inválida!');
+            }
+
+            if (resultado != null) {
+                console.log('O resultado da operação é: ', resultado);
+            }
+
+            leitor.close();
+        });
+    });
+});
