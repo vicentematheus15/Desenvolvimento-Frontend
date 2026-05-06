a52b4487b218a4097ae1232048eda974355737a7
vicentematheus15 <vicentematheus1507@gmail.com>
2026-05-06 17:40:09 -0300

    feat: refactor event listeners to use a single function for context changes and update banner image

--- JavaScript/Manipulando elementos no DOM/Fokus-projeto-base/script.js
+++ JavaScript/Manipulando elementos no DOM/Fokus-projeto-base/script.js
@@ -2,19 +2,36 @@ const html = document.querySelector('html');
 const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
-const banner = document.querySelector('.app__image')
+const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
 
 focoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco')
-    banner.setAttribute('src', './imagens/foco.png')
+    alterarContexto('foco')
 });
 
 curtoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto')
-    banner.setAttribute('src', './imagens/descanso-curto.png')
+    alterarContexto('descanso-curto')
 });
 
 longoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo')
-    banner.setAttribute('src', './imagens/descanso-longo.png')
+    alterarContexto('descanso-longo')
 });
+
+function alterarContexto(contexto){
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `./imagens/${contexto}.png`)
+    switch (contexto) {
+        case 'foco':
+            titulo.innerHTML = `
+            Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
+            break;
+        
+        case 'descanso-curto': {
+            
+        }
+        default:
+            break;
+    }
+}
