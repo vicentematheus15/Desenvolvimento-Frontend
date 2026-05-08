df97a44c9ae13ad702f08ed78e98c9775ca66125
vicentematheus15 <vicentematheus1507@gmail.com>
2026-05-08 18:14:04 -0300

    feat: update timer display format and adjust time settings for focus and breaks

--- JavaScript/Manipulando elementos no DOM/Fokus-projeto-base/script.js
+++ JavaScript/Manipulando elementos no DOM/Fokus-projeto-base/script.js
@@ -29,21 +29,25 @@ musicaFocoInput.addEventListener('change', () => {
 });
 
 focoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 1500
     alterarContexto('foco')
     focoBt.classList.add('active')
 });
 
 curtoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 300
     alterarContexto('descanso-curto')
     curtoBt.classList.add('active')
 });
 
 longoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 900
     alterarContexto('descanso-longo')
     longoBt.classList.add('active')
 });
 
 function alterarContexto(contexto){
+    mostrarTempo()
     botoes.forEach((contexto) => {
         contexto.classList.remove('active')
     })
@@ -106,8 +110,9 @@ function zerar(){
 }
 
 function mostrarTempo(){
-    const tempo = tempoDecorridoEmSegundos
-    tempoNaTela.innerHTML = `${tempo}`
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000)
+    const tempoFormatado = tempo.toLocaleTimeString('pt-BR', {minute: '2-digit', second: '2-digit'})
+    tempoNaTela.innerHTML = `${tempoFormatado}`
 }
 
 mostrarTempo()
