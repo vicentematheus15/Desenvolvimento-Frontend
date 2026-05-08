203806ecf7322da8c85dcf0f9cf1f05499da4dc3
vicentematheus15 <vicentematheus1507@gmail.com>
2026-05-08 15:57:03 -0300

    feat: enhance button functionality by adding active class and refactoring context change logic

--- JavaScript/Manipulando elementos no DOM/Fokus-projeto-base/script.js
+++ JavaScript/Manipulando elementos no DOM/Fokus-projeto-base/script.js
@@ -4,20 +4,27 @@ const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button')
 
 focoBt.addEventListener('click', () => {
     alterarContexto('foco')
+    focoBt.classList.add('active')
 });
 
 curtoBt.addEventListener('click', () => {
     alterarContexto('descanso-curto')
+    curtoBt.classList.add('active')
 });
 
 longoBt.addEventListener('click', () => {
     alterarContexto('descanso-longo')
+    longoBt.classList.add('active')
 });
 
 function alterarContexto(contexto){
+    botoes.forEach((contexto) => {
+        contexto.classList.remove('active')
+    })
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `./imagens/${contexto}.png`)
     switch (contexto) {
@@ -28,9 +35,17 @@ function alterarContexto(contexto){
             `
             break;
         
-        case 'descanso-curto': {
+        case 'descanso-curto':
+            titulo.innerHTML = `
+                Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            ` 
+            break;
+
+        case "descanso-longo":
+            titulo.innerHTML = `
+                Hora de voltar à superfície.<strong class="app__title-strong"> Faça uma pausa longa.</strong>
+            `
             
-        }
         default:
             break;
     }
