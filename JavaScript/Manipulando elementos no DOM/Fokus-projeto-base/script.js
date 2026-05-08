993e770abca64fb47457f5c6328193ed24d2291e
vicentematheus15 <vicentematheus1507@gmail.com>
2026-05-08 17:22:33 -0300

    feat: rename audio variables for clarity and add audio feedback for countdown completion

--- JavaScript/Manipulando elementos no DOM/Fokus-projeto-base/script.js
+++ JavaScript/Manipulando elementos no DOM/Fokus-projeto-base/script.js
@@ -8,9 +8,9 @@ const botoes = document.querySelectorAll('.app__card-button');
 const startPauseBt = document.querySelector('#start-pause')
 const musicaFocoInput = document.querySelector('#alternar-musica');
 const musica = new Audio('./sons/luna-rise-part-one.mp3');
-const somPlay = new Audio('./sons/play.wav');
-const somPause = new Audio('./sons/pause.mp3');
-const somBeep = new Audio('./sons/beep.mp3');
+const audioPlay = new Audio('./sons/play.wav');
+const audioPause = new Audio('./sons/pause.mp3');
+const audioTempoFinalizado = new Audio('./sons/beep.mp3');
 
 let tempoDecorridoEmSegundos = 5
 let intervaloId = null
@@ -72,20 +72,25 @@ function alterarContexto(contexto){
 
 const contagemRegressiva = () => {
     if(tempoDecorridoEmSegundos <= 0){
-        zerar()
+        audioTempoFinalizado.play()
         alert('tempo finalizado')
+        zerar()
         return
     }
     tempoDecorridoEmSegundos -= 1
+    console.log('Tempo: ' + tempoDecorridoEmSegundos)
+    console.log('Id: ' + intervaloId)
 }
 
 startPauseBt.addEventListener('click', iniciarOuPausar)
 
 function iniciarOuPausar(){
     if(intervaloId){
+        audioPause.play()
         zerar()
         return
     }
+    audioPlay.play()
     intervaloId = setInterval(contagemRegressiva, 1000)
 }
 
