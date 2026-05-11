c36ff9ff23de5b8d7f2b8f197723fd9d9207d282
Matheus Vicente <vicentematheus1507@gmail.com>
2026-05-11 17:13:56 -0300

    fix: stringify tasks before saving to local storage

--- JavaScript/Manipulando elementos no DOM/Fokus-projeto-base/script-crud.js
+++ JavaScript/Manipulando elementos no DOM/Fokus-projeto-base/script-crud.js
@@ -16,5 +16,5 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
         descricao: textArea.value
     }
     tarefas.push(tarefa)
-    localStorage.setItem('tarefas', tarefas)
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
 })
