7fbfe3c716af781d93a5815a1a74aa4126f35c94
Matheus Vicente <vicentematheus1507@gmail.com>
2026-05-11 16:55:01 -0300

    feat: implement task addition button functionality to toggle task form visibility

---
+++ JavaScript/Manipulando elementos no DOM/Fokus-projeto-base/script-crud.js
@@ -0,0 +1,8 @@
+//encontrar botao de adicionar tarefa
+
+const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
+const formAdicionarTarefa = document.querySelector('.app__form-add-task')
+
+btnAdicionarTarefa.addEventListener('click', () => {
+    formAdicionarTarefa.classList.toggle('hidden')
+})
