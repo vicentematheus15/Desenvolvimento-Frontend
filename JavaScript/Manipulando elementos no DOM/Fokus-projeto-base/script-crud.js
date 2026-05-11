f042f6017961620fe8ba706044475a74429119ac
Matheus Vicente <vicentematheus1507@gmail.com>
2026-05-11 17:02:56 -0300

    feat: add task submission functionality and update local storage handling

--- JavaScript/Manipulando elementos no DOM/Fokus-projeto-base/script-crud.js
+++ JavaScript/Manipulando elementos no DOM/Fokus-projeto-base/script-crud.js
@@ -1,8 +1,20 @@
 //encontrar botao de adicionar tarefa
 
-const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
-const formAdicionarTarefa = document.querySelector('.app__form-add-task')
+const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
+const formAdicionarTarefa = document.querySelector('.app__form-add-task');
+const textArea = document.querySelector('.app__form-textarea');
+
+const tarefas = []
 
 btnAdicionarTarefa.addEventListener('click', () => {
     formAdicionarTarefa.classList.toggle('hidden')
+})
+
+formAdicionarTarefa.addEventListener('submit', (evento) => {
+    evento.preventDefault
+    const tarefa = {
+        descricao: textArea.value
+    }
+    tarefas.push(tarefa)
+    localStorage.setItem('tarefas', tarefas)
 })
